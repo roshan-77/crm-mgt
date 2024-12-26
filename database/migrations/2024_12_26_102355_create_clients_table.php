596bb68d298b9269e7464461114c58f63dc776b3
roshan-77 <roshan.khadka.u@gmail.com>
2024-12-26 22:09:34 +1100

    [Client] Created migration files

---
+++ database/migrations/2024_12_26_102355_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50); 
+            $table->string('number', 15);
+            $table->boolean('company'); //If a project is from company(1) or an individual(0) client.
+            $table->string('address', 50);
+            $table->string('referred_by', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('/users', [UserController::class, 'index']);
+
+
+Route::put('/users/{id}',[UserController::class, 'update']);
+Route::get('/users/create',[UserController::class, 'create']);
+
+Route::get('/users/{id}',[UserController::class, 'show']);
+
+Route::post('/users',[UserController::class, 'store']);
