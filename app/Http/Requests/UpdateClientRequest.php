169aec9eb87cc89a95de5f62e70ebbcc4a360742
PrashantDhungana <sharma.prashant2000@gmail.com>
2024-12-28 14:22:38 +1100

    added validation req

---
+++ app/Http/Requests/UpdateClientRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class UpdateClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email', 'max:50'],
+            'number' => ['required', 'string', 'max:15'],
+            'company' => ['required', 'boolean'],
+            'address' => ['string', 'max:50'],
+            'referred_by' => ['string', 'max:50'],
+        ];
+        
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
